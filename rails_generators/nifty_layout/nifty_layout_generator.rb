8f8a8a4e9dd5e5b913ad97ff05edd9b8ef180887
David Blankenship <dtblankenship@gmail.com>
2009-05-21 15:45:07 -0400

    Updated to IU template

--- rails_generators/nifty_layout/nifty_layout_generator.rb
+++ rails_generators/nifty_layout/nifty_layout_generator.rb
@@ -16,10 +16,17 @@ class NiftyLayoutGenerator < Rails::Generator::Base
         m.file     "stylesheet.sass",  "public/stylesheets/sass/#{file_name}.sass"
       else
         m.template "layout.html.erb", "app/views/layouts/#{file_name}.html.erb"
+        # stylesheets
         m.file     "stylesheet.css",  "public/stylesheets/#{file_name}.css"
-        m.file     "iupui.css", "public/stylesheets/iupui.css"
+        m.file     "print.css", "public/stylesheets/print.css"
+        m.file     "screen.css", "public/stylesheets/screen.css"
+        m.file     "styles.css", "public/stylesheets/styles.css"
+        m.file     "ie6.css", "public/stylesheets/ie6.css"
+        # images
         m.file     "iu_crimson.gif", "public/images/iu_crimson.gif"
         m.file     "iupui-footer.gif", "public/images/iupui-footer.gif"
+        m.file     "iu_white.gif", "public/images/iu_white.gif"
+        m.file     "blockiu_white.gif", "public/images/blockiu_white.gif"
       end
       m.file "helper.rb", "app/helpers/layout_helper.rb"
     end
