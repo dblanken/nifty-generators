c5d062a7ec29be06c20fa3a784056344cb8c2359
David Blankenship <dblanken@iupui.edu>
2010-11-11 09:22:28 -0500

    Updated rails3 version to IU stylesheet/layout

--- Rakefile
+++ Rakefile
@@ -8,19 +8,4 @@ Cucumber::Rake::Task.new(:features) do |t|
 end
 
 task :default => :features
-require 'rubygems'
-require 'rake'
-require 'echoe'
-
-Echoe.new('nifty-generators', '0.2.4iupui') do |p|
-  p.project        = "niftygenerators"
-  p.description    = "A collection of useful generator scripts for Rails."
-  p.url            = "http://github.com/ryanb/nifty-generators"
-  p.author         = 'Ryan Bates and David Blankenship'
-  p.email          = "dblanken (at) iupui (dot) edu"
-  p.ignore_pattern = ["script/*"]
-  p.development_dependencies = []
-end
-
-Dir["#{File.dirname(__FILE__)}/tasks/*.rake"].sort.each { |ext| load ext }
 
--- lib/generators/nifty/layout/layout_generator.rb
+++ lib/generators/nifty/layout/layout_generator.rb
@@ -14,6 +14,16 @@ module Nifty
         else
           template 'layout.html.erb', "app/views/layouts/#{file_name}.html.erb"
           copy_file 'stylesheet.css', "public/stylesheets/#{file_name}.css"
+          copy_file 'print.css', "public/stylesheets/print.css"
+          copy_file "screen.css", "public/stylesheets/screen.css"
+          copy_file "styles.css", "public/stylesheets/styles.css"
+          copy_file "ie6.css", "public/stylesheet/ie6.css"
+          copy_file "rails.css", "public/stylesheet/rails.css"
+          
+          copy_file "iu_crimson.gif", "public/images/iu_crimson.gif"
+          copy_file "iupui-footer.gif", "public/images/iupui-footer.gif"
+          copy_file "iu_white.gif", "public/images/iu_white.gif"
+          copy_file "blockiu_white.gif", "public/images/blockiu_white.gif"
         end
         copy_file 'layout_helper.rb', 'app/helpers/layout_helper.rb'
         copy_file 'error_messages_helper.rb', 'app/helpers/error_messages_helper.rb'
